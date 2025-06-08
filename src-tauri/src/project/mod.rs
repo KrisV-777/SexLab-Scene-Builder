d17c0d7f97639ba78b0dd0de19d8d5311c08d0c8
Scrabx3 <kris.vlntn@gmail.com>
2025-06-08 10:32:10 +0200

    fix assertion to accept either valid id length

--- src-tauri/src/project/mod.rs
+++ src-tauri/src/project/mod.rs
@@ -1,5 +1,5 @@
-use serde::{Serialize, Deserialize};
 use crate::project::serialize::EncodeBinary;
+use serde::{Deserialize, Serialize};
 
 // A collection of scenes and various meta data, such as author of the project
 pub mod package;
@@ -24,8 +24,8 @@ pub struct NanoID(pub String);
 
 impl NanoID {
     const NANOID_ALPHABET: [char; 36] = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     ];
     const PREFIX_HASH_LEN: usize = 4;
     const NANOID_LENGTH: usize = 8;
@@ -39,14 +39,24 @@ impl NanoID {
     }
 
     fn new(len: usize) -> Self {
-        assert!(len == Self::NANOID_LENGTH || len == Self::PREFIX_HASH_LEN, "NanoID length must be either {} or {}", Self::NANOID_LENGTH, Self::PREFIX_HASH_LEN);
+        assert!(
+            len == Self::NANOID_LENGTH || len == Self::PREFIX_HASH_LEN,
+            "NanoID length must be either {} or {}",
+            Self::NANOID_LENGTH,
+            Self::PREFIX_HASH_LEN
+        );
         NanoID(nanoid::nanoid!(len, &Self::NANOID_ALPHABET))
     }
 }
 
 impl EncodeBinary for NanoID {
     fn get_byte_size(&self) -> usize {
-        assert_eq!(self.0.len(), Self::NANOID_LENGTH);
+        assert!(
+            self.0.len() == Self::NANOID_LENGTH || self.0.len() == Self::PREFIX_HASH_LEN,
+            "NanoID length must be either {} or {}",
+            Self::NANOID_LENGTH,
+            Self::PREFIX_HASH_LEN
+        );
         self.0.len()
     }
 

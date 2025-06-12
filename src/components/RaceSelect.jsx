5fb70ed6ed36d48799dcf7d4bdd8b87ce4fbbaa4
Scrabx3 <kris.vlntn@gmail.com>
2025-06-12 12:26:07 +0200

    isolate components

---
+++ src/components/RaceSelect.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { Select } from "antd";
+import { invoke } from "@tauri-apps/api/core"
+
+function RaceSelect({ race, onSelect }) {
+  const [raceKeys, setRaceKeys] = useState([]);
+
+  useEffect(() => {
+    invoke('get_race_keys').then(result => setRaceKeys(result));
+  }, []);
+
+  return (
+    <Select
+      className="position-race-select"
+      defaultValue={race}
+      showSearch
+      placeholder="Race"
+      optionFilterProp="children"
+      filterOption={(input, option) =>
+        (option?.label ?? '').includes(input)
+      }
+      filterSort={(optionA, optionB) =>
+        (optionA?.label ?? '')
+          .toLowerCase()
+          .localeCompare((optionB?.label ?? '').toLowerCase())
+      }
+      options={raceKeys.map((race, i) => {
+        return { value: race, label: race };
+      })}
+      onSelect={(value) => {
+        onSelect(value);
+      }}
+    />
+  );
+}
+
+export default RaceSelect;
